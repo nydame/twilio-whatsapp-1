e250439e254515c282547fd67e3b1f0d442974c6
Felicia Betancourt <fmbetancourt@gmail.com>
2024-04-24 13:59:03 -0700

    Refactor; get WhatsApp messaging working.

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ require('dotenv').config();
 var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
+const shop_order = `Order #00001 was created or updated on your online shop. Tap https://oneearthsacredarts.com/wp-admin/edit.php?post_type=shop_order view the order.`;
 
 app.get('/', (req, res) => {
-    sendSMSToMyself("Ahoy, me scurvy mates!");
-  res.send('Ahoy, me scurvy mates!');
+  sendSMSToMyself(shop_order);
+  res.send('Ahoy, ye scurvy dogs!!!');
 });
 
 app.listen(port, () => {
@@ -22,9 +23,11 @@ function sendSMSToMyself(msg) {
   client.messages
     .create({
       body: msg,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: process.env.MY_PHONE_NUMBER
+      contentSid: 'HX0aa17e5d2ad6bf9ab1e3944d2e3e9b49',
+      // contentVariables: JSON.stringify({ 1: '99999' }),
+      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+      to: `whatsapp:${process.env.CHARLIES_PHONE_NUMBER}`
     })
-    .then(message => console.log(message.sid))
+    .then(message => {console.log(message.sid); console.log(message.error_code); console.log(message.error_message);console.log(message.status); console.log(message.uri);})
     .catch(err => console.error(err));
 }
